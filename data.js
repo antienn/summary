8178a3b37906a5958b2fa9ad0a0a2a6582712983
Trần An Tiến <89237543+supertien12@users.noreply.github.com>
2023-03-15 23:51:11 +0700

    add

---
+++ data.js
@@ -0,0 +1,38 @@
+var listweb =[
+    {
+        "id":"0",
+        "name": "xe tăng cướp bóng",
+        "image": "./sources/Cinema.png",
+        "url":"https://nincode2907.github.io/webFilm/"
+    },
+    {
+        "id":"1",
+        "name": "Beast",
+        "image": "./sources/Beast.png",
+        "url":"https://frost1129.github.io/beast-mpc-final/"
+    },
+    {
+        "id":"2",
+        "name": "Kẻ hủy diệt c++",
+        "image": "./sources/Film24h.png",
+        "url":"https://antien.quythanh.tk/"
+    },
+    {
+        "id":"3",
+        "name": "TBOG",
+        "image": "./sources/CircleofLife.png",
+        "url":"https://hieungocnguyen.github.io/FinalRound_WebDesignContest_TBOG/"
+    },
+    {
+        "id":"4",
+        "name": "Đội 7",
+        "image": "./sources/Vercel_app.png",
+        "url":"https://cuoc-thi-web-20220326.vercel.app/"
+    },
+    {
+        "id":"5",
+        "name": "Đội 4",
+        "image": "./sources/Team4(2).png",
+        "url":"https://workanhnguyen.github.io/landingpagedoi4mpc/"
+    },
+]
